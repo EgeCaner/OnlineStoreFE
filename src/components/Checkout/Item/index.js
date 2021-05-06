9e8f08521dacdce1ec7cd3fc65197e2063bfacc1
s21bariscan <bariscan@sabanciuniv.edu>
2021-05-06 16:44:02 +0300

    little fixes in cart

--- src/components/Checkout/Item/index.js
+++ src/components/Checkout/Item/index.js
@@ -1,35 +1,29 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { removeCartItem, addProduct, reduceCartItem } from './../../../redux/Cart/cart.actions';
+import React from "react"
+import {useDispatch} from "react-redux"
+import {
+  removeCartItem,
+  addProduct,
+  reduceCartItem,
+} from "./../../../redux/Cart/cart.actions"
 
 const Item = (product) => {
-  const dispatch = useDispatch();
-  const {
-    productName,
-    productThumbnail,
-    productPrice,
-    quantity,
-    documentID
-  } = product;
+  const dispatch = useDispatch()
+  const {productName, imageUrl, price, quantity, productId} = product
 
-  const handleRemoveCartItem = (documentID) => {
+  const handleRemoveCartItem = (productId) => {
     dispatch(
       removeCartItem({
-        documentID
+        productId,
       })
-    );
-  }
-
-  const handleAddProduct = (product) => {
-    dispatch(
-      addProduct(product)
     )
   }
 
+  const handleAddProduct = (product) => {
+    dispatch(addProduct(product))
+  }
+
   const handleReduceItem = (product) => {
-    dispatch(
-      reduceCartItem(product)
-    );
+    dispatch(reduceCartItem(product))
   }
 
   return (
@@ -37,36 +31,31 @@ const Item = (product) => {
       <tbody>
         <tr>
           <td>
-            <img src={productThumbnail} alt={productName} />
+            <img src={imageUrl} alt={productName} />
           </td>
+          <td>{productName}</td>
           <td>
-            {productName}
-          </td>
-          <td>
-            <span className="cartBtn"
-              onClick={() => handleReduceItem(product)}>
+            <span className="cartBtn" onClick={() => handleReduceItem(product)}>
               {`< `}
             </span>
-            <span>
-              {quantity}
-            </span>
-            <span className="cartBtn"
-              onClick={() => handleAddProduct(product)}>
+            <span>{quantity}</span>
+            <span className="cartBtn" onClick={() => handleAddProduct(product)}>
               {` >`}
             </span>
           </td>
-          <td>
-            £{productPrice}
-          </td>
+          <td>£{price}</td>
           <td align="center">
-            <span className="cartBtn remove" onClick={() => handleRemoveCartItem(documentID)}>
+            <span
+              className="cartBtn remove"
+              onClick={() => handleRemoveCartItem(productId)}
+            >
               X
             </span>
           </td>
         </tr>
       </tbody>
     </table>
-  );
+  )
 }
 
-export default Item;
+export default Item
--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,73 +1,63 @@
-import React, { useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProductStart, setProduct } from './../../redux/Products/products.actions';
-import { addProduct } from './../../redux/Cart/cart.actions';
-import Button from './../forms/Button';
-import './styles.scss';
+import React, {useEffect} from "react"
+import {useParams, useHistory} from "react-router-dom"
+import {useDispatch, useSelector} from "react-redux"
+import {
+  fetchProductStart,
+  setProduct,
+} from "./../../redux/Products/products.actions"
+import {addProduct} from "./../../redux/Cart/cart.actions"
+import Button from "./../forms/Button"
+import "./styles.scss"
 
-const mapState = state => ({
-  product: state.productsData.product
-});
+const mapState = (state) => ({
+  product: state.productsData.product,
+})
 
 const ProductCard = ({}) => {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const { productID } = useParams();
-  const { product } = useSelector(mapState);
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const {productId} = useParams()
+  const {product} = useSelector(mapState)
 
-  const {
-    productThumbnail,
-    productName,
-    productPrice,
-    productDesc,
-  } = product;
+  const {imageUrl, productName, price, description} = product
 
   useEffect(() => {
-    dispatch(
-      fetchProductStart(productID)
-    )
+    dispatch(fetchProductStart(productId))
 
     return () => {
-      dispatch(
-        setProduct({})
-      )
+      dispatch(setProduct({}))
     }
-
-  }, []);
+  }, [])
 
   const handleAddToCart = (product) => {
-    if (!product) return;
-    dispatch(
-      addProduct(product)
-    );
-    history.push('/cart');
+    if (!product) return
+    dispatch(addProduct(product))
+    history.push("/cart")
   }
 
   const configAddToCartBtn = {
-    type: 'button'
+    type: "button",
   }
 
   return (
     <div className="productCard">
       <div className="hero">
-        <img src={productThumbnail} />
+        <img src={imageUrl} />
       </div>
       <div className="productDetails">
         <ul>
           <li>
-            <h1>
-              {productName}
-            </h1>
+            <h1>{productName}</h1>
           </li>
           <li>
-            <span>
-              £{productPrice}
-            </span>
+            <span>£{price}</span>
           </li>
           <li>
             <div className="addToCart">
-              <Button {...configAddToCartBtn} onClick={() => handleAddToCart(product)}>
+              <Button
+                {...configAddToCartBtn}
+                onClick={() => handleAddToCart(product)}
+              >
                 Add to cart
               </Button>
             </div>
@@ -75,12 +65,13 @@ const ProductCard = ({}) => {
           <li>
             <span
               className="desc"
-              dangerouslySetInnerHTML={{ __html: productDesc }} />
+              dangerouslySetInnerHTML={{__html: description}}
+            />
           </li>
         </ul>
       </div>
     </div>
-  );
+  )
 }
 
-export default ProductCard;
+export default ProductCard
